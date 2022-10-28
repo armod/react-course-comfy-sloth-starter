93a3fedabd0786932a2ee620bc90718813cd0f7a
armod <armods@gmail.com>
2022-10-28 20:26:26 +0200

    update

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,8 @@
-export const formatPrice = () => {}
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat('pl-PL', {
+    style: 'currency',
+    currency: 'PLN',
+  }).format(number / 100)
+}
 
 export const getUniqueValues = () => {}
