55e78bc2c3f6c1d5a6c92ff94a1125c9b0cba97e
armod <armods@gmail.com>
2022-10-28 10:26:38 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -3,10 +3,45 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { Navbar, Sidebar, Footer } from './components'
 import styled from 'styled-components'
 
-const Button = styled.button``
+const Button = styled.button`
+  background: green;
+  color: white;
+`
+
+const Container = styled.div`
+  background: red;
+  color: white;
+  .hero {
+    font-size: 8rem;
+  }
+`
+const Container2 = styled.div`
+  background: red;
+  color: white;
+  .hero {
+    font-size: 1rem;
+  }
+`
 
 function App() {
-  return <h4>comfy sloth starter</h4>
+  return (
+    <div>
+      <h4>comfy sloth starter</h4>
+      <Button>click me</Button>
+      <Container>
+        <div>
+          <h3>hello world</h3>
+        </div>
+        <div className='hero'>hero texts</div>
+      </Container>
+      <Container2>
+        <div>
+          <h3>hello world</h3>
+        </div>
+        <div className='hero'>hero texts</div>
+      </Container2>
+    </div>
+  )
 }
 
 export default App
