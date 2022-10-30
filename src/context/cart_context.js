3b63177ddcd292aaaa7acd1c33b57ffd61d6444d
armod <armods@gmail.com>
2022-10-30 22:42:19 +0100

    update

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -20,8 +20,15 @@ const CartContext = React.createContext()
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(cart_reducer, initialState)
+
+  //add to cart
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
+  }
   return (
-    <CartContext.Provider value='cart context'>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state, addToCart }}>
+      {children}
+    </CartContext.Provider>
   )
 }
 // make sure use
