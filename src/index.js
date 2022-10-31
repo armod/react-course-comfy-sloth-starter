81f5156b6fa8635b1c0b732f148bcd93a170a3ff
armod <armods@gmail.com>
2022-10-31 15:44:21 +0100

    update

--- src/index.js
+++ src/index.js
@@ -7,17 +7,27 @@ import { FilterProvider } from './context/filter_context'
 import { CartProvider } from './context/cart_context'
 import { UserProvider } from './context/user_context'
 import { Auth0Provider } from '@auth0/auth0-react'
-
+//domain dev-6x1vjgpryfcbbxgx.us.auth0.com
+//client id 8pIIJvfwsVysXe68WBI51X4YubshHiCI
 // ReactDOM.render(<App />, document.getElementById('root'))
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   // <React.StrictMode>
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain='dev-6x1vjgpryfcbbxgx.us.auth0.com'
+    clientId='8pIIJvfwsVysXe68WBI51X4YubshHiCI'
+    redirectUri={window.location.origin}
+    cacheLocation='localstorage'
+  >
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
+  </Auth0Provider>
   // </React.StrictMode>
 )
